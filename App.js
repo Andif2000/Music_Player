e401746fe4d2d4363811c360dadbff4ff28da890
Andi Fahmi <andifahmi266@gmail.com>
2023-08-21 23:22:03 +0800

    Upgrade App

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Music_Player from './Src/Screens/Music_Player';
+import MusicPlayer from './Src/Screens/Music_Player';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Music_Player />
+      <MusicPlayer />
     </View>
   );
 }
